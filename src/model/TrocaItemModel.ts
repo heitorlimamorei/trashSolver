b7e8f16d102d80df6ecdc57ec6418ae9a6e06ac8
heitorlimamorei <61924603+heitorlimamorei@users.noreply.github.com>
2022-10-07 18:34:04 -0300

    modified:   src/components/template/ListaDeTipos.tsx 	modified:   src/model/TrocaItemModel.ts 	modified:   src/pages/trocas/index.tsx

--- src/model/TrocaItemModel.ts
+++ src/model/TrocaItemModel.ts
@@ -1,53 +1,85 @@
-export default class TrocaItemModel{
-    #id:string;
-    #nome:string;
-    #descricao:string;
-    #photourl:string;
-    #quemcriou:string;
-    #tipo:string;
-    #trocado:boolean;
-    #listadeinteresses: string[];
-    #trocaRealizadaId: string;
-    constructor(id:string,nome:string,descricao:string,photourl:string,quemcriou:string,tipo:string,trocado:boolean,listadeinteresses: string[], trocaRealizadaId:string){
-        this.#id = id;
-        this.#nome = nome;
-        this.#descricao = descricao;
-        this.#photourl =photourl;
-        this.#quemcriou = quemcriou;
-        this.#tipo = tipo;
-        this.#trocado = trocado;
-        this.#listadeinteresses = listadeinteresses;
-        this.#trocaRealizadaId = trocaRealizadaId;
-    }
-    static TrocaItemEmBraco(){
-        return new TrocaItemModel(" "," "," "," "," "," ", false, ["","",""], " ")
-    }
-    get id():string{
-        return this.#id;
-    }
-    get nome():string{
-        return this.#nome;
-    }
-    get descricao():string{
-        return this.#descricao;
-    }
-    get Photourl():string{
-        return this.#photourl;
-    }
-    get quemcriou():string{
-        return this.#quemcriou;
-    }
-    get tipo():string{
-        return this.#tipo;
-    }
-    get trocado():boolean{
-        return this.#trocado;
-    }
-    get listadeinteresses(): string[]{
-        return this.#listadeinteresses;
-    }
-    get trocaRealizadaId(): string{
-        return this.#trocaRealizadaId;
-    }
-
+export default class TrocaItemModel {
+  #id: string;
+  #nome: string;
+  #descricao: string;
+  #photourl: string;
+  #quemcriou: string;
+  #tipo: string;
+  #trocado: boolean;
+  #listadeinteresses: string[];
+  #trocaRealizadaId: string;
+  constructor(
+    id: string,
+    nome: string,
+    descricao: string,
+    photourl: string,
+    quemcriou: string,
+    tipo: string,
+    trocado: boolean,
+    listadeinteresses: string[],
+    trocaRealizadaId: string
+  ) {
+    this.#id = id;
+    this.#nome = nome;
+    this.#descricao = descricao;
+    this.#photourl = photourl;
+    this.#quemcriou = quemcriou;
+    this.#tipo = tipo;
+    this.#trocado = trocado;
+    this.#listadeinteresses = listadeinteresses;
+    this.#trocaRealizadaId = trocaRealizadaId;
+  }
+  static fromJSON(troca) {
+    return new TrocaItemModel(
+      troca.id,
+      troca.nome,
+      troca.descricao,
+      troca.photourl,
+      troca.quemcriou,
+      troca.tipo,
+      troca.trocado,
+      troca.listadeinteresses,
+      troca.trocaRealizadaId
+    );
+  }
+  static TrocaItemEmBraco() {
+    return new TrocaItemModel(
+      " ",
+      " ",
+      " ",
+      " ",
+      " ",
+      " ",
+      false,
+      ["", "", ""],
+      " "
+    );
+  }
+  get id(): string {
+    return this.#id;
+  }
+  get nome(): string {
+    return this.#nome;
+  }
+  get descricao(): string {
+    return this.#descricao;
+  }
+  get Photourl(): string {
+    return this.#photourl;
+  }
+  get quemcriou(): string {
+    return this.#quemcriou;
+  }
+  get tipo(): string {
+    return this.#tipo;
+  }
+  get trocado(): boolean {
+    return this.#trocado;
+  }
+  get listadeinteresses(): string[] {
+    return this.#listadeinteresses;
+  }
+  get trocaRealizadaId(): string {
+    return this.#trocaRealizadaId;
+  }
 }
--- src/pages/trocas/index.tsx
+++ src/pages/trocas/index.tsx
@@ -1,12 +1,30 @@
 import Layout from '../../components/template/Layout'
-
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import TrocaItemModel from '../../model/TrocaItemModel'
 
 export default function Trocas() {
+  const [trocas, setTrocas] = useState<TrocaItemModel[]>([])
+  async function getTrocas() {
+    const resp = await axios.get('/api/trocas')
+    return await resp.data.map(troca => TrocaItemModel.fromJSON(troca))
+  }
+  async function loadTrocas(){
+    const resp = await getTrocas()
+    setTrocas(resp) 
+  }
+  useEffect(()=>{
+    loadTrocas()
+  }, [])
   return (
     <div className={``}>
-      <Layout titulo="Noticações" subtitulo="Aqui você vai gerenciar suas notificações">
+      <Layout titulo="Trocas" subtitulo="Aqui você vai ver as trocas disponiveis">
         <div>
-            FEED de trocas
+          <ul>
+          {trocas.map((troca:TrocaItemModel) => <li>
+            {troca.nome} || {troca.descricao}
+          </li>)}
+          </ul>
             
         </div>
       </Layout>
