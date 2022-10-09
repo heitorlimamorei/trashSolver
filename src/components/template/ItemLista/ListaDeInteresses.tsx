f66597d5ff1f05c3a893057177583e5bdfb99880
Felipe Rese <feliperese2018@gmail.com>
2022-10-09 14:56:12 -0300

    modified:   src/components/template/ItemLista/ListaDeInteresses.tsx 	modified:   src/components/template/ItemLista/index.tsx

--- src/components/template/ItemLista/ListaDeInteresses.tsx
+++ src/components/template/ItemLista/ListaDeInteresses.tsx
@@ -2,38 +2,13 @@ import {motion} from 'framer-motion'
 import {ToogleIcon} from '../../icons/Icones'
 import React from "react"
 
-const ListaDeInteresses = (props) => {
-    const [isHover, toggleHover] = React.useState(false);
-    const toggleHoverMenu = () => {
-      toggleHover(!isHover);
-    }
 
-    const subMenuAnimate = {
-        enter: {
-          opacity: 1,
-          rotateX: 0,
-          transition: {
-            duration: 0.5
-          },
-          display: "block"
-        },
-        exit: {
-          opacity: 0,
-          rotateX: -15,
-          transition: {
-            duration: 0.5,
-            delay: 0.3
-          },
-          transitionEnd: {
-            display: "none"
-          }
-        }
-      };
+const ListaDeInteresses = (props) => {
     return (
         <motion.div
             className="h-1/6"
-            onHoverStart={toggleHoverMenu}
-            onHoverEnd={toggleHoverMenu}>
+            onHoverStart={props.onHoverStart}
+            onHoverEnd={props.onHoverEnd}>
             
             <div className="flex flex-row">
                 <a className="text-sm m-1 mt-3">Lista de Interesses</a>
@@ -43,10 +18,9 @@ const ListaDeInteresses = (props) => {
             <motion.div
               className="bg-green-600 rounded-xl m-2"
               initial="exit"
-              animate={isHover ? "enter" : "exit"}
-              variants={subMenuAnimate}
+              animate={props.condition ? "enter" : "exit"}
+              variants={props.animation}
             >
-              <div className="sub-menu-background" />
               <div className="sub-menu-container">
                 <ul>
                     <li>{props.listaInteresses}</li>
--- src/components/template/ItemLista/index.tsx
+++ src/components/template/ItemLista/index.tsx
@@ -4,10 +4,44 @@ import ConteudoItem from "./ConteudoItem"
 import ImagemItem from "./ImagemItem"
 import BotaoTroca from "./BotaoTroca"
 import BotaoChat from "./BotaoChat"
+import React from 'react'
 
+interface ItemListapProps {
+  Nome: string;
+  Descricao: string;
+  onhoverStart: () => void;
+  onhoverEnd: () => void;
+  condition: boolean;
+  animation: Animation;
+}
 
 const ItemLista = (props) => {
-    
+  const [isHover, toggleHover] = React.useState(false);
+  const toggleHoverMenu = () => {
+    toggleHover(!isHover);
+  }
+
+  const subMenuAnimate = {
+      enter: {
+        opacity: 1,
+        rotateX: 0,
+        transition: {
+          duration: 0.5
+        },
+        display: "block"
+      },
+      exit: {
+        opacity: 0,
+        rotateX: -15,
+        transition: {
+          duration: 0.5,
+          delay: 0.3
+        },
+        transitionEnd: {
+          display: "none"
+        }
+      }
+    };
 return (
 
   <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{duration: 0.8, delay: 0.5, ease: [0, 0.71, 0.2, 1.01]}} className="bg-indigo-50 shadow-xl text-gray-900 dark:bg-gray-300 rounded-xl  grid  grid-cols-10  grid-rows-1 w-100 h-1/6 my-4 dark:text-gray-900">
@@ -16,7 +50,12 @@ return (
 
     <ConteudoItem Nome ={props.Nome} Descricao = {props.Descricao} />
 
-    <ListaDeInteresses/>
+    <ListaDeInteresses 
+      onHoverStart = {toggleHoverMenu} 
+      onHoverEnd = {toggleHoverMenu}
+      condition = {isHover}
+      animation = {subMenuAnimate}
+    />
 
     <BotaoTroca/>
 
