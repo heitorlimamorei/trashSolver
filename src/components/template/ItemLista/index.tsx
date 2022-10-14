517d102628a7ca755df4ef47ce245a45315f001b
Felipe Rese <feliperese2018@gmail.com>
2022-10-14 16:54:07 -0300

    modified:   src/components/template/ItemLista/index.tsx

--- src/components/template/ItemLista/index.tsx
+++ src/components/template/ItemLista/index.tsx
@@ -41,7 +41,7 @@ const ItemLista = (props:ItemListapProps) => {
     };
 return (
 
-  <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{duration: 0.8, delay: 0.5, ease: [0, 0.71, 0.2, 1.01]}} className="bg-indigo-50 shadow-xl text-gray-900 dark:bg-gray-300 rounded-xl  grid  grid-cols-10  grid-rows-1 w-100 h-1/6 my-4 dark:text-gray-900">
+  <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{duration: 0.8, delay: 0.5, ease: [0, 0.71, 0.2, 1.01]}} className="bg-indigo-50 shadow-xl text-gray-900 dark:bg-gray-300 rounded-xl  grid  grid-cols-10  grid-rows-1 w-full h-1/6 my-4 dark:text-gray-900">
 
     <ImagemItem/>
 
