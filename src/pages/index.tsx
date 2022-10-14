8a2ec767315aa360b72dddb553e54b73af24ee20
Felipe Rese <feliperese2018@gmail.com>
2022-10-14 16:17:03 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/pages/index.tsx     modified:   yarn.lock

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,15 +6,7 @@ export default function Home() {
   return (
     <div className={``}>
       <Layout titulo = '' subtitulo = ''>
-        <nav className=' bg-green-600 flex justify-center align-center'>
-
-          <div className='w-100 h-100 flex  justify-self-center self-center '> {/* circle */}
-            <button className=" bg-green-800 text-gray-300 w-20 h-20 rounded-full flex items-center justify-center self-center -36 ">
-              {menu}
-            </button>
-          </div>
-
-        </nav>
+        <MenuPie/>
       </Layout>
 
     </div>
