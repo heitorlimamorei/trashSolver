1ff2033ae47623eadbf8ab42de0ecf0cf7a2b5bc
heitorlimamorei <heitorlmoreira2017@outlook.com>
2022-10-24 15:40:26 +0000

    algumas adições

--- src/model/Variaveis.ts
+++ src/model/Variaveis.ts
@@ -1,2 +1,2 @@
-const baseUrl = 'https://3000-heitorlimam-trashsolver-ermfp40fnfv.ws-us72.gitpod.io'
+const baseUrl = 'https://3000-heitorlimam-trashsolver-esrvnxd7slg.ws-us72.gitpod.io'
 export default baseUrl
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
-import LayoutHome from '../components/template/LayoutHome'
+import LayoutHome from '../components/template/Home/LayoutHome'
 import BotaoHome from '../components/template/Home/BotaoHome'
 import {ponto} from '../components/icons/Icones'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const router = useRouter()
   return (
     <div className={``}>
       <LayoutHome titulo= "Home" subtitulo="Bem-Vindo ao TrashSolver" >
         <div className="w-full h-full flex flex-row">
-          <BotaoHome Nome='Ponto de Coleta' icon={ponto}/>
+          <BotaoHome onClick={()=> {
+            router.push('/pontoDeColetas')
+          }} Nome='Ponto de Coleta' icon={ponto}/>
         </div>        
       </LayoutHome>
     </div>
