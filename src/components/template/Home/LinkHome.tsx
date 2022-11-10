f8fdc653f0c6c72d661568fd7d8e1906d9441fb2
Felipe Rese <feliperese2018@gmail.com>
2022-11-10 20:56:01 -0300

    modified:   src/components/template/Home/LinkHome.tsx 	modified:   src/model/Variaveis.ts 	modified:   src/pages/Paralelos/1A.tsx 	modified:   src/pages/index.tsx

--- src/components/template/Home/LinkHome.tsx
+++ src/components/template/Home/LinkHome.tsx
@@ -3,7 +3,7 @@ import ImagemHome from './ImagemHome'
 
 export default function LinkHome(props) {
     return(
-        <div className="flex flex-col md:w-[49%] md:h-[100%] w-full bg-indigo-50 dark:bg-gray-600 dark:text-white rounded-xl">
+        <div className="flex flex-col  md:w-[40%] md:h-[100%] w-full bg-indigo-50 dark:bg-gray-600 dark:text-white rounded-xl">
             <ImagemHome/>
             <ConteudoHome titulo={props.titulo} Descricao={props.descricao}/>
         </div> 
